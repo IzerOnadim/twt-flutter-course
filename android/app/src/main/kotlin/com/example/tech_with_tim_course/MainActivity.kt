d94e1b23a0882660608a392b7b671ad5149c904e
Izer Onadim <izeronadim@gmail.com>
2022-04-24 14:27:36 +0100

    Initial commit: tutorials 1 & 2 complete

---
+++ android/app/src/main/kotlin/com/example/tech_with_tim_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tech_with_tim_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TWT App',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Hello World!")),
+      body: Column(
+        children: const <Widget>[TestWidget(), TestWidget(), TestWidget()],
+      ),
+    );
+  }
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello World!");
+  }
+}
