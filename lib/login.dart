947fcdd285a4d92de29072b006a24bf4b72ad9bd
Izer Onadim <izeronadim@gmail.com>
2022-04-30 14:38:54 +0100

    Tutorials 8 & 9 finished

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
+import 'auth.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -19,37 +21,54 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String name = "";
-  TextEditingController controller = TextEditingController();
+  User? user;
+  String displayName = "";
+
+  @override
+  void initState() {
+    super.initState();
+    Authentication.signOut(context: context);
+  }
 
   void submit() {
-    name = controller.text;
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => HomePage(name)));
+    Authentication.signInWithGoogle(context: context).then((user) => {
+          this.user = user,
+          displayName = (user != null && user.displayName != null
+              ? user.displayName
+              : "Unknown user")!,
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => HomePage(displayName)))
+        });
+  }
+
+  Widget googleLoginButton() {
+    return OutlinedButton(
+      onPressed: submit,
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        splashFactory: InkSplash.splashFactory,
+        side: const BorderSide(color: Colors.grey),
+        padding: const EdgeInsets.all(10),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: const <Widget>[
+          Image(image: AssetImage("assets/google_logo.png"), height: 35),
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              "Sign in with Google",
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-        alignment: Alignment.center,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.person),
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.done),
-                splashColor: Colors.blueGrey,
-                tooltip: "Submit",
-                onPressed: submit,
-              ),
-              labelText: "Type your name:",
-              border: const OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.blue, width: 2),
-              ),
-            ),
-          ),
-        ));
+    return Align(alignment: Alignment.center, child: googleLoginButton());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login.dart';
 
-void main() => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Firebase.initializeApp();
     return MaterialApp(
       title: 'TWT App',
       theme: ThemeData(
